160a5cea6d202bea8c5ed520d8f70fc7df92cddd
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2023-06-21 19:58:37 +0600

    fixed problem

--- pages/all-blogs/index.js
+++ pages/all-blogs/index.js
@@ -77,7 +77,7 @@ const index = () => {
     return (
         <div className="mid-container">
             <div className="mt-5 mb-5">
-                <h1 className="text-4xl text-center font-bold">All Blogs</h1>
+                <h1 className="text-4xl text-center font-bold ">All Blogs</h1>
                 {/* <p className='text-sm text-center mt-2'>Travel, inspiration and tips for your next adventure. Discover the world with us!</p> */}
             </div>
 
